9d7773fb1573de30e377536cd0b5eb0b1802562a
ぴーまん <70518694+P-man2976@users.noreply.github.com>
2023-07-15 11:03:06 +0000

    Add fetch, set up cron, untrack node modules

--- src/worker.ts
+++ src/worker.ts
@@ -1,50 +1,60 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export interface Env {
+export type Bindings = {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-	//
-	// Example binding to a D1 Database. Learn more at https://developers.cloudflare.com/workers/platform/bindings/#d1-database-bindings
-	// DB: D1Database
-}
+	
+	HATO_STATUS: KVNamespace;
+};
+
+const servers = [
+	{
+		name: 'Hato',
+		url: 'https://hato.cf',
+	},
+	{
+		name: 'Hato API',
+		url: 'https://api.hato.cf',
+	},
+];
+
+const app = new Hono<{ Bindings: Bindings }>();
+app.use('*', cors());
+
+app.get('/', async (c) => {
+	return c.json(await c.env.HATO_STATUS.get('status', 'json'));
+});
 
 export default {
+	fetch: app.fetch,
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
+	async scheduled(
+		event: ScheduledEvent,
+		env: Bindings,
+		ctx: ExecutionContext
+	): Promise<void> {
 		// A Cron Trigger can make requests to other endpoints on the Internet,
 		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const status = await Promise.all(
+			servers.map(async ({ name, url }) => {
+				const res = await fetch(url);
+
+				return {
+					name,
+					ok: res.ok,
+					status: res.status,
+					statusText: res.statusText,
+					body: await res.text(),
+				};
+			})
+		);
+
+		await env.HATO_STATUS.put(
+			'status',
+			JSON.stringify({ status, updatedAt: new Date(event.timeStamp) })
+		);
 	},
 };
