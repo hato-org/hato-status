0af3769986f0909fb43677355e11c46bb4b5e82a
P-man2976 <ikmy1119924@gmail.com>
2023-07-23 02:03:31 +0900

    Add history feature

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
-import { Bindings, Info } from './worker';
 import { cors } from 'hono/cors';
+import { HTTPException } from 'hono/http-exception';
+import { Bindings, Info } from './worker';
 
 export const app = new Hono<{ Bindings: Bindings }>();
 app.use('*', cors());
@@ -11,7 +12,7 @@ app.get('/', async (c) => {
 
 app
 	.get('/info', async (c) => {
-		return c.json(await c.env.HATO_STATUS.get('info', 'json') ?? []);
+		return c.json((await c.env.HATO_STATUS.get('info', 'json')) ?? []);
 	})
 	.post(async (c) => {
 		const info = await c.env.HATO_STATUS.get<Info[]>('info', 'json');
@@ -23,3 +24,17 @@ app
 			)
 		);
 	});
+
+app.get('/history', async (c) => {
+	const { id, page } = c.req.query();
+
+	if (!id || !page) throw new HTTPException(400);
+
+	return c.json(
+		(await c.env.DB.prepare(
+			`SELECT * FROM status_${id} ORDER BY timestamp DESC LIMIT ?1 OFFSET ?2`
+		)
+			.bind(24, 24 * Number(page))
+			.all()).results
+	);
+});
--- src/worker.ts
+++ src/worker.ts
@@ -5,6 +5,11 @@ export type Bindings = {
 	// MY_KV_NAMESPACE: KVNamespace;
 
 	HATO_STATUS: KVNamespace;
+
+	// Example binding to a D1 Database. Learn more at https://developers.cloudflare.com/workers/platform/bindings/#d1-database-bindings
+	// DB: D1Database
+
+	DB: D1Database;
 };
 
 export type Server = {
@@ -23,10 +28,12 @@ export type Info = {
 
 const servers = [
 	{
+		id: 'hato',
 		name: 'Hato',
 		url: 'https://hato.cf',
 	},
 	{
+		id: 'hato_api',
 		name: 'Hato API',
 		url: 'https://api.hato.cf',
 	},
@@ -45,22 +52,41 @@ export default {
 		// publish to a Queue, query a D1 Database, and much more.
 
 		const status = await Promise.all(
-			servers.map(async ({ name, url }) => {
+			servers.map(async ({ id, name, url }) => {
+				const start = Date.now();
 				const res = await fetch(url);
 
 				return {
+					id,
 					name,
 					ok: res.ok,
 					status: res.status,
 					statusText: res.statusText,
 					body: await res.text(),
+					time: Date.now() - start,
 				};
 			})
 		);
 
+		// Store status data to KV
 		await env.HATO_STATUS.put(
 			'status',
-			JSON.stringify({ status, updatedAt: new Date(event.scheduledTime).toISOString() })
+			JSON.stringify({
+				status,
+				updatedAt: new Date(event.scheduledTime).toISOString(),
+			})
 		);
+
+		// Add status data to history (only HH:00)
+		if (!new Date(event.scheduledTime).getMinutes())
+			await Promise.all(
+				status.map(async ({ id, status, statusText, time, ok }) =>
+					env.DB.prepare(
+						'INSERT INTO ?1 (status, statusText, responseTime, ok, timestamp) VALUES (?2, ?3, ?4, ?5, ?6, ?7)'
+					)
+						.bind(id, status, statusText, time, ok, event.scheduledTime)
+						.all()
+				)
+			);
 	},
 };
