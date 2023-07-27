123a5d1eaaaf301510b5c4bf7d91c258c2cbaf38
P-man2976 <ikmy1119924@gmail.com>
2023-07-28 00:24:00 +0900

    Validation with zod

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
-import { HTTPException } from 'hono/http-exception';
+import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 import { Bindings, Info, servers } from './worker';
 
 export const app = new Hono<{ Bindings: Bindings }>();
@@ -16,34 +17,52 @@ app
 	.get('/info', async (c) => {
 		return c.json((await c.env.HATO_STATUS.get('info', 'json')) ?? []);
 	})
-	.post(async (c) => {
-		const info = await c.env.HATO_STATUS.get<Info[]>('info', 'json');
+	.post(
+		zValidator(
+			'json',
+			z.object({
+				type: z.literal('maintenance'),
+				title: z.string(),
+				description: z.string(),
+				startAt: z.string().datetime(),
+				endAt: z.string().datetime(),
+				scope: z.string().array().nonempty(),
+			})
+		),
+		async (c) => {
+			const info = await c.env.HATO_STATUS.get<Info[]>('info', 'json');
+
+			return c.json(
+				await c.env.HATO_STATUS.put(
+					'info',
+					JSON.stringify([...(info ?? []), await c.req.json()])
+				)
+			);
+		}
+	);
+
+app.get(
+	'/history',
+	zValidator(
+		'query',
+		z.object({
+			id: z.string().refine((val) => servers.some(({ id }) => id === val)),
+			page: z.string().regex(/^[0-9]+$/),
+		})
+	),
+	async (c) => {
+		const { id, page } = c.req.query();
+
+		const offset = Number(page) - 1;
 
 		return c.json(
-			await c.env.HATO_STATUS.put(
-				'info',
-				JSON.stringify([...(info ?? []), await c.req.json()])
-			)
+			(
+				await c.env.DB.prepare(
+					`SELECT * FROM status_${id} ORDER BY timestamp DESC LIMIT ?1 OFFSET ?2`
+				)
+					.bind(24, 24 * offset)
+					.all()
+			).results
 		);
-	});
-
-app.get('/history', async (c) => {
-	const { id, page } = c.req.query();
-
-	// 既知のID以外のリクエストを拒否 (SQLインジェクションの防止)
-	const serverId = servers.find((server) => server.id === id)?.id;
-
-	if (!serverId || !page) throw new HTTPException(400);
-
-	const offset = Number(page) - 1;
-
-	return c.json(
-		(
-			await c.env.DB.prepare(
-				`SELECT * FROM status_${serverId} ORDER BY timestamp DESC LIMIT ?1 OFFSET ?2`
-			)
-				.bind(24, 24 * offset)
-				.all()
-		).results
-	);
-});
+	}
+);
